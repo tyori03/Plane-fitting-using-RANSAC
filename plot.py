d550fc33b557a4cc8e30885628c0c51f24fe6684
tyori <accelworld0302@gmail.com>
2020-12-18 19:11:34 +0900

    update: for Open3d v0.7 or later For debugging by Open3d v0.7 or later.

--- plot.py
+++ plot.py
@@ -7,15 +7,14 @@ from mpl_toolkits.mplot3d import Axes3D
 
 import ransac
 
-
-pcd = o3d.read_point_cloud("./pc_color_chair.pcd")
-o3d.draw_geometries([pcd])
+pcd = o3d.io.read_point_cloud("./pc_color_chair.pcd")
+o3d.visualization.draw_geometries([pcd])
 
 a, b, d = ransac.find_plane(pcd)
 print(ransac.angle_rotate(a, b, d))
 
-downpcd = o3d.voxel_down_sample(pcd, voxel_size=0.02)
-xyz = np.asarray(downpcd.points)
+down_pcd = pcd.voxel_down_sample(voxel_size=0.02)
+xyz = np.asarray(down_pcd.points)
 
 min_x = np.amin(xyz[:, 0])
 max_x = np.amax(xyz[:, 0])
@@ -24,13 +23,13 @@ max_y = np.amax(xyz[:, 1])
 
 x = np.arange(min_x, max_x)
 y = np.arange(min_y, max_y)
-X,Y = np.meshgrid(x,y)
-Z = a*X + b*Y + d
+X, Y = np.meshgrid(x, y)
+Z = a * X + b * Y + d
 
 fig = plt.figure()
 ax = fig.gca(projection='3d')
 
-ax.plot_surface(X,Y,Z)
+ax.plot_surface(X, Y, Z)
 ax.scatter(xyz[:, 0], xyz[:, 1], xyz[:, 2])
 
 plt.show()
--- ransac.py
+++ ransac.py
@@ -10,40 +10,67 @@ from mpl_toolkits.mplot3d import Axes3D
 
 
 def find_plane(pcd):
+    """
+    get coefficient
+
+    Parameters
+    ----------
+    pcd : open3d.geometry.PointCloud
+        PointCloudData
+
+    Returns
+    -------
+    a, d, d : float
+        Coefficients of the plane equation(Z = aX + bY + d).
+    """
+
     xyz = np.asarray(pcd.points)
 
     # 説明変数
-    XY = xyz[:, :2]
+    xy = xyz[:, :2]
     # 目的関数
-    Z = xyz[:, 2]
+    z = xyz[:, 2]
     ransac = linear_model.RANSACRegressor(residual_threshold=0.01)
 
-    ransac.fit(XY, Z)
-    a, b = ransac.estimator_.coef_  # 係数
-    d = ransac.estimator_.intercept_  # 切片
+    ransac.fit(xy, z)
+    a, b = ransac.estimator_.coef_  # coefficients
+    d = ransac.estimator_.intercept_  # intercept
 
-    print(a, b, d)
+    # print(a, b, d)
     return a, b, d  # Z = aX + bY + d
 
 
 def angle_rotate(a, b, d):
+    """
+    get angle of rotation
+
+    Parameters
+    ----------
+    a, b, d : float
+        Coefficients of the plane equation(Z = aX + bY + d).
+
+    Returns
+    -------
+    rad - math.pi : float
+        angle of rotation
+    """
+
     x = np.arange(30)
     y = np.arange(30)
-    X, Y = np.meshgrid(x, y)
-    Z = a * X + b * Y + d
-    rad = math.atan2(Y[1][0] - Y[0][0], (Z[1][0] - Z[0][0]))
+    x, y = np.meshgrid(x, y)
+    z = a * x + b * y + d
+    rad = math.atan2(y[1][0] - y[0][0], (z[1][0] - z[0][0]))
     return rad - math.pi
 
 
-def show_graph(X, Y, Z):
+def show_graph(x, y, z):
     fig = plt.figure()
     ax = fig.gca(projection='3d')
-    ax.plot_surface(X, Y, Z)
+    ax.plot_surface(x, y, z)
 
     plt.show()
 
 
 if __name__ == '__main__':
-
-    pcd = o3d.read_point_cloud("./data/pc_color_chair.pcd")
-    find_plane(pcd)
+    sample_pcd = o3d.io.read_point_cloud("./pc_color_chair.pcd")
+    print(find_plane(sample_pcd))
